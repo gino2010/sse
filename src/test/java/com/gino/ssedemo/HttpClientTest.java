8bf7f66014dc7182b86926bdf65c7e8405ac24c9
Gino Zhao <ginozhao2010@gmail.com>
2018-02-22 10:44:41 +0800

    remove comment code

--- src/test/java/com/gino/ssedemo/HttpClientTest.java
+++ src/test/java/com/gino/ssedemo/HttpClientTest.java
@@ -11,8 +11,6 @@ import org.junit.runners.JUnit4;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-import static org.apache.http.HttpHeaders.USER_AGENT;
-
 /**
  * http client test
  *
@@ -29,8 +27,6 @@ public class HttpClientTest {
         HttpClient client = HttpClientBuilder.create().build();
         HttpGet request = new HttpGet(url);
 
-        // add request header
-//         request.addHeader("User-Agent", USER_AGENT);
         HttpResponse response = client.execute(request);
 
         System.out.println("Response Code : "
@@ -39,10 +35,8 @@ public class HttpClientTest {
         BufferedReader rd = new BufferedReader(
                 new InputStreamReader(response.getEntity().getContent()));
 
-        // StringBuffer result = new StringBuffer();
-        String line = "";
+        String line;
         while ((line = rd.readLine()) != null) {
-            // result.append(line);
             System.out.println(line);
         }
     }
